450e3b1a62d38ee1de2280284e9997e91093f17c
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2019-08-27 15:14:51 +0300

    master: ...

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,10 @@ const { NODE_ENV } = process.env;
 let config = null;
 
 switch (NODE_ENV) {
+  case 'production': {
+    config = require('./dev');
+    break;
+  }
   case 'development': {
     config = require('./dev');
     break;
